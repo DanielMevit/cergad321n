53dcd4e82149aad3357105124ee52e60e35895b3
Daniel <daniel.mitev@hotmail.com>
2017-09-09 16:02:18 +0200

    Initial commit

---
+++ dea-in/DEA-IN_MK/js/custom.js
@@ -0,0 +1,127 @@
+
+// google map
+var map = '';
+var center;
+
+function initialize() {
+    var skopje = {lat: 41.979843, lng: 21.436816};
+    var mapOptions = {
+      zoom: 16,
+      center: new google.maps.LatLng(41.979843, 21.436816),
+      scrollwheel: false
+    };
+  
+    map = new google.maps.Map(document.getElementById('map-canvas'),  mapOptions);
+
+    google.maps.event.addDomListener(map, 'idle', function() {
+        calculateCenter();
+    });
+  
+    google.maps.event.addDomListener(window, 'resize', function() {
+        map.setCenter(center);
+    });
+    var marker = new google.maps.Marker({
+          position: skopje,
+          map: map
+        });
+}
+
+function calculateCenter() {
+  center = map.getCenter();
+}
+
+function loadGoogleMap(){
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' + 'callback=initialize';
+    document.body.appendChild(script);
+}
+
+// Flexslider
+$(function(){
+  /* FlexSlider */
+  $('.flexslider').flexslider({
+      animation: "fade",
+      directionNav: false
+  });
+
+  new WOW().init();
+
+  // Magnific Pop up for Portfolio section
+  $('.portfolio-container').magnificPopup({
+    delegate: '.portfolio-popup', // child items selector, by clicking on it popup will open
+    type: 'image',
+    gallery: {
+      enabled: true
+    }    
+  });
+
+  loadGoogleMap();
+});
+
+// isotope
+jQuery(document).ready(function($){
+
+  if ( $('.iso-box-wrapper').length > 0 ) { 
+
+      var $container  = $('.iso-box-wrapper'), 
+        $imgs     = $('.iso-box img');
+
+      $container.imagesLoaded(function () {
+
+        $container.isotope({
+        layoutMode: 'fitRows',
+        itemSelector: '.iso-box'
+        });
+
+        $imgs.load(function(){
+          $container.isotope('reLayout');
+        })
+
+      });
+
+      //filter items on button click
+      $('.filter-wrapper li a').click(function(){
+
+          var $this = $(this), filterValue = $this.attr('data-filter');
+
+      $container.isotope({ 
+        filter: filterValue,
+        animationOptions: { 
+            duration: 750, 
+            easing: 'linear', 
+            queue: false, 
+        }                
+      });             
+
+      // don't proceed if already selected 
+      if ( $this.hasClass('selected') ) { 
+        return false; 
+      }
+
+      var filter_wrapper = $this.closest('.filter-wrapper');
+      filter_wrapper.find('.selected').removeClass('selected');
+      $this.addClass('selected');
+
+        return false;
+      }); 
+
+  }
+
+});
+
+// Hide mobile menu after clicking on a link
+    $('.navbar-collapse a').click(function(){
+        $(".navbar-collapse").collapse('hide');
+    });
+
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+    $(function() {
+        $('.navbar-default a, a,').bind('click', function(event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top - 68
+            }, 1000);
+            event.preventDefault();
+        });
+    });
